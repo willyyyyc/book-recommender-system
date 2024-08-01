2a1de6c267dfa99bc8b4bac952a3468a2ec85896
willyyyyc <willcarmichaelcf@gmail.com>
2024-08-01 12:18:03 -0300

    Debug load data script, add options to read_csv

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -10,6 +10,13 @@ def load_csv_files(directory):
     for filename in os.listdir(directory):
         if filename.endswith('.csv'):
             filepath = os.path.join(directory, filename)
-            df = pd.read_csv(filepath)
+            df = pd.read_csv(filepath, sep=';',
+                                       on_bad_lines='skip',
+                                       encoding='latin-1',
+                                       dtype='object')
             dataframes.append(df)
     return dataframes
+
+if __name__ == "__main__":
+    load_csv_files('data/raw/')
+    print('Successful loading of data.')
